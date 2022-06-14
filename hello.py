f59d9e1a46f9462252c99a5f9c6fd352f344e23d
Kirtikalia <98168309+Kirtikalia@users.noreply.github.com>
2022-06-14 15:02:35 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
+#single line printing 
 print("yoooooo")
+
+#multiple line printing 
+print(''' In the dark blue sky you keep,
+And often through my curtains peep,
+For you never shut your eye,
+‘Till the sun is in the sky.
+'''
+)
