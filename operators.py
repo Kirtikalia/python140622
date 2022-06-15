85c64e0a4c75092ee90503a1389c12c302e1b19d
Kirtikalia <98168309+Kirtikalia@users.noreply.github.com>
2022-06-15 11:22:06 +0530

    operator functions

---
+++ operators.py
@@ -0,0 +1,47 @@
+#operators 
+#arithmatic operator = + , -, *, /, % etc 
+# assignments operators = =, +=, -=, == etc
+# comaprison operator = ==, >, <, >=, <=, != etc
+# logical operators = and, or, not etc 
+
+#variables declarations 
+#a = 6
+#b = 9 
+
+# arithmatic operators 
+#print("the value of 6+9", a+b)
+#print("the value of 6-9", 6-9)
+#print("the value of 6*9", 6*9)
+#print("the value of 6/9", 6/9)
+#print("the value of 6%9", 6%9)
+
+#assignment operators 
+#a = 6 
+#a -= 3
+#a += 7 # a = 7+6
+#a *= 2 # a = 2*6
+#a /=2
+#a %=2
+
+#print(a)
+
+#comparison operator 
+#a = 14 
+#b = 2 
+
+#b = (14==2)
+#print(b)
+
+#b = (14!=2)
+#print(b)
+
+#logical operator 
+bool1 = True 
+bool2 = False
+
+print("the value of bool1 and bool 2 is", (bool1 and bool2))
+print("the value of bool1 or bool 2 is", (bool1 or bool2))
+print("the value of not bool2 is", (not bool2))
+
+
+
---
+++ variable.py
@@ -0,0 +1,41 @@
+#variable declaration 
+a = "hello world how are yoiu "
+b = 45
+c = 45.6
+d = False
+e = None
+
+#printing the values
+print()
+print(b)
+print(c)
+print(d)
+print(e)
+
+# type checking 
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))#print(type(e))
+
+#ways to write string 
+A = "hello world "
+_f = 'hello world'
+g = '''hello alok  'amit' how are you'''
+
+#printing the values 
+print(A)
+print(_f)
+print(g)
+
+#type checking 
+print(type(A))
+print(type(_f))
+print(type(g))
+
+#rules of writing variable 
+#valid names 
+#gsg, _regj, 
+# invalid names 
+#56ghh, alok kalia , "redgjl"
+
