515473b9a55a619a677673f510d403185b544e61
Kirtikalia <98168309+Kirtikalia@users.noreply.github.com>
2022-06-18 09:42:32 +0530

    Create condition operators.py

---
+++ condition operators.py
@@ -0,0 +1,87 @@
+#syntax
+#if  (condition):
+#statement
+
+#program
+
+#x = 5 
+#y = 7 
+#if x<y:
+ #   print("x is less than y")
+
+#f(x!=y):
+  #  print("x is not equal to y")
+
+#if(y>x):
+ #   print("y is greater")
+
+
+#syntax
+#if Condition:
+    #statement
+#else :
+    #statement
+
+#x = 5 
+#y = 7 
+#if x>y:
+ #   print("x is greater than y")
+#else:
+ #   print("y is greater than x")
+
+#a = int(input("enter any number:"))
+#if(a>50):
+ #   print("value is greater than 50")
+#else:
+#    print("value is less than 50")
+
+#nested if syntax
+
+#if  condition1:
+    #if condition2:
+        #statement:
+
+#x=5
+#if x<=10:
+ #   if x>=5:
+  #      print("x is greater than equal to 5 and less than equal to 10")
+   # if x==5:
+    #    print("x is greater to 5")
+    #else:
+     #   print("x is not equal to 5")
+
+#a = 45
+#if (a>45):
+ #   print("gsjhgcj")
+#else:
+ #   print("your input is wrong")    
+
+#elif conditions
+#if condition:
+    #statement1......
+#elif condition:
+    #statement2......
+#elif condition:
+    #statement3......
+
+a=9
+if a<=10:
+    print("a is less than equals to 10")
+elif a<5:
+    print("x is less than 5")
+elif a!=9:
+    print("a is not equals to 9")
+else:
+    print("everything is wrong")
+
+
+
+
+
+
+
+
+
+
+
+
